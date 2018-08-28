5c00821453af760c0842fcc3830c207aea1abf77
jhoni-costa <jhoni_costa@outlook.com>
2018-08-28 15:06:23 -0300

    Criando entity Post com User alinhado

---
+++ src/main/java/br/com/jhonicosta/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,13 @@
+package br.com.jhonicosta.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.jhonicosta.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post	, String>{
+
+	
+	
+}
